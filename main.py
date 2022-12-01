bc012e40e9bdc83a94e70cdd9ca76e7f9f1cb1c1
qiemanqieman <1324137924@qq.com>
2022-12-01 19:57:47 +0800

    Time: test time

--- main.py
+++ main.py
@@ -18,27 +18,27 @@ def parse_args():
                         default=
                         # "VSIDS"
                         # "ERWA"
-                        "RSR"
+                        # "RSR"
                         # "LRB"
-                        # "CHB"
+                        "CHB"
                         )
     parser.add_argument("-i", "--input", type=str, help="specify the CNF file needed to be solved, default and1.cnf",
                         default=
                         # "examples/and1.cnf"
                         # "examples/and2.cnf"
-                        # "examples/bmc-2.cnf"
+                        "examples/bmc-2.cnf"
                         # "examples/bmc-7.cnf"
                         # "my-examples/good-16-vars.cnf"
                         # "my-examples/bad-12-vars.cnf"
-                        "examples/bmc-1.cnf"
+                        # "examples/bmc-1.cnf"
                         # "my-examples/test.cnf"
                         # "my-examples/track-main-2018/2d5cc23d8d805a0cf65141e4b4401ba4-20180322_164245263_p_cnf_320_1120.cnf"
                         # "my-examples/track-main-2018/3c92dedae9bea8c2c22acd655e33d52d-e_rphp065_05.cnf"
                         )
     parser.add_argument("-r", "--restart-policy", type=str, choices=["MLR"],
                         help="specify the restart policy, default to be None, default None", default=
-                        None
-                        # "MLR"
+                        # None
+                        "MLR"
                         )
 
     return parser.parse_args()
